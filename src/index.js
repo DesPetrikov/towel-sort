edac9d4d3c39b1ba84fc81f0f2a46c11b0644915
DesPetrikov <desik1603@gmail.com>
2021-12-06 20:50:29 +0300

    feat: add second solution to towelSort

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@
 
 module.exports = function towelSort(matrix) {
     if (!matrix || !matrix.length) return [];
-    let newArr = [];
-    matrix.forEach((el, index) => {
-        if (index % 2 === 0) {
-            for (let i = 0; i < el.length; i++) {
-                newArr.push(el[i]);
-            }
-        } else {
-            for (let i = el.length - 1; i >= 0; i--) {
-                newArr.push(el[i]);
-            }
-        }
-    });
-    return newArr;
+	return matrix.map((el, index) => {
+		return (index % 2) ? el.reverse() : el
+	}).flat()
 };
